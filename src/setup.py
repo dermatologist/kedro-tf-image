a78824fa6b6edafd79b3d1ca23aa1b36fae8063e
Bell Eapen <github@gulfdoctor.net>
2021-05-15 18:26:40 -0400

    update version

--- src/setup.py
+++ src/setup.py
@@ -46,7 +46,7 @@ with open("requirements.txt", "r", encoding="utf-8") as f:
 
 setup(
     name="kedro_tf_image",
-    version="0.2",
+    version="0.3.0",
     packages=find_packages(exclude=["tests"]),
     entry_points={"console_scripts": [entry_point]},
     install_requires=requires,
